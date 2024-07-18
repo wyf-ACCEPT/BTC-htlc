5e8270a0b4e2a15e6d6f939cb6dcb29dfafedf1c
wyf-ACCEPT <define_wifi@163.com>
2024-07-17 23:15:32 -0700

    -

--- scripts/generate-wallet.js
+++ scripts/generate-wallet.js
@@ -3,8 +3,9 @@ const bip39 = require('bip39')
 const { Wallet } = require('ethers')
 
 async function main() {
-  const w = Wallet.createRandom()
-  console.log(w)
+  const wallet = Wallet.createRandom()
+  console.log(wallet)
+  console.log(wallet.privateKey)
   let words = bip39.generateMnemonic(128)
   console.log(words)
 }
